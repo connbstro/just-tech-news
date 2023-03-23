66c7643cdd4c3492c68423fa295cdbe1a2ec4bef
Connor S <constroh@gmail.com>
2023-03-23 17:21:09 -0500

    index.js assocations

--- models/index.js
+++ models/index.js
@@ -1,7 +1,20 @@
-// Require User model
+// Require User model.
 const User = require("./User");
 // Require Post model
 const Post = require("./Post");
 
-// Export Post & User models
+// Create associations.
+// This association creates the reference for the id column in the User model
+// to link to the corresponding foreign key pair, which is the user_id in the Post model.
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+// Reverse association.
+// Defining the relationship of the Post model to the User. Can only belong to one user.
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+// Export Post & User models.
 module.exports = { User, Post };
